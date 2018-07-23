04907f0b56be1612d2ec8a6d98adb94518c5b4e1
GRODNO\Anton_Sokolovskii <a.sokolovsky15@gmail.com>
2018-07-23 13:44:19 +0300

    s

--- server.js
+++ server.js
@@ -3,13 +3,13 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+cost PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello 1111\n');
+    res.send('Hello sadsads\n');
 });
 
 app.listen(PORT, HOST);
