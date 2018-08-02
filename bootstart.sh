774a276bfad86b35492fe542abd081c7e5b822e1
GRODNO\Anton_Sokolovskii <a.sokolovsky15@gmail.com>
2018-08-02 15:39:22 +0300

    up

--- bootstart.sh
+++ bootstart.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-find . -type f -name '*.js'  
-export JS_FILES="$(find . -type f -name '*.js'  )"
-for file in $JS_FILES
+FILES=*.js
+for file in $FILES
 do
     node --check $file
 done
